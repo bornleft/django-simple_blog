b5557ad650a5e644262e112efa0392f8a8f2d5e7
Ruslan Tarasov <ruslan.tarasov2@gmail.com>
2011-07-21 01:56:29 +0400

    only test

--- project/apps/blog/models.py
+++ project/apps/blog/models.py
@@ -8,7 +8,7 @@ import os
 
 class Group(models.Model):
     name = models.CharField(_(u"Название"), max_length=100, null=False, blank=False )
-
+		
 class Tag(models.Model):
     name = models.CharField(_(u"Название"), max_length = 100, null = False, blank = False)
     entrys = models.ManyToManyField('Entry', verbose_name=_(u"Запись"), blank=True, null=True)
@@ -26,3 +26,8 @@ class Entry(models.Model):
     author = models.ForeignKey(User, verbose_name=_(u"Автор"), null=False, blank=False)
     date_pub = models.DateTimeField(_(u"Дата опубликования"), auto_now_add=True)
     date_change = models.DateTimeField(_(u"Дата изменения"), auto_now=True)
+
+
+class Test():
+		print 'first str'
+		print 'last str'

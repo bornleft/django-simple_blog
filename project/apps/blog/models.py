eebcb70d3015ad9987357b92ea2ba2b2a3825341
Ruslan Tarasov <ruslan.tarasov2@gmail.com>
2011-07-21 03:45:05 +0400

    add lines verbose_name*

--- project/apps/blog/models.py
+++ project/apps/blog/models.py
@@ -8,16 +8,27 @@ import os
 
 class Group(models.Model):
     name = models.CharField(_(u"Название"), max_length=100, null=False, blank=False )
-		
+
+		class Meta:
+				verbose_name = 'группа'
+				verbose_name_plural = 'группы'
+
 class Tag(models.Model):
     name = models.CharField(_(u"Название"), max_length = 100, null = False, blank = False)
     entrys = models.ManyToManyField('Entry', verbose_name=_(u"Запись"), blank=True, null=True)
 
+		class Meta:
+				verbose_name = 'тэг'
+				verbose_name_plural = 'тэги'
 
 class Comment(models.Model):
     entry = models.ForeignKey('Entry', verbose_name = u"", null = False, blank = False)
     comment = models.TextField(_(u"Название"), max_length = 100, null = False, blank = False)
     date_pub = models.DateTimeField(u"Дата опубликования", default=datetime.now())
+
+		class Meta:
+				verbose_name = 'коммент'
+				verbose_name_plural = 'комменты'
 
 class Entry(models.Model):
     group = models.ForeignKey(Group, verbose_name=_(u""), null=False, blank=False)
@@ -26,3 +37,7 @@ class Entry(models.Model):
     author = models.ForeignKey(User, verbose_name=_(u"Автор"), null=False, blank=False)
     date_pub = models.DateTimeField(_(u"Дата опубликования"), auto_now_add=True)
     date_change = models.DateTimeField(_(u"Дата изменения"), auto_now=True)
+
+		class Meta:
+				verbose_name = 'запись'
+				verbose_name_plural = 'записи'

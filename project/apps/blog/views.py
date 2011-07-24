98795afb8a13cb6571eaeac7b3c8e56b290df7e9
Ruslan Tarasov <ruslan.tarasov2@gmail.com>
2011-07-25 00:47:15 +0400

    modify main.html

--- project/apps/blog/views.py
+++ project/apps/blog/views.py
@@ -10,6 +10,9 @@ from project.apps.blog.models import Tag, Group, Comment, Entry
 def first(request):
 	entrys = Entry.objects.all().order_by('-date_pub')
 	groups = Group.objects.all().order_by('name')
+	
+	for entry in entrys:
+		print entry.date_pub, entry.date_change
 	return render_to_response('blog/main.html', locals(), context_instance=RequestContext(request))
 
 

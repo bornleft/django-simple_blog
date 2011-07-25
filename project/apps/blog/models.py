f281a2b4d36c981110f405ac7041c2ec3c0c0d0d
Alex K <st00nsa@gmail.com>
2011-07-25 14:04:53 +0400

    + get_tags method

--- project/apps/blog/models.py
+++ project/apps/blog/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import logging
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import ugettext as _
@@ -44,6 +45,12 @@ class Entry(models.Model):
 		author = models.ForeignKey(User, verbose_name=_(u"Автор"), null=False, blank=False)
 		date_pub = models.DateTimeField(_(u"Дата опубликования"), auto_now_add=True)
 		date_change = models.DateTimeField(_(u"Дата изменения"), auto_now=True)
+
+		def get_tags(self):
+			tags = Tag.objects.filter(entrys__in = [self])
+			return tags
+
+
 
 		def __unicode__(self):
 			return u'%s' % (self.name)
